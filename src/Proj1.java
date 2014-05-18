319c78a73cf84afcd40564cf7c62c55420ad7b0a
Lem <lemmie6@gmail.com>
2014-05-19 08:08:26 +1000

    testing data read

--- src/Proj1.java
+++ src/Proj1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Andrew
@@ -9,6 +11,13 @@ public class Proj1 {
     static final Double EPSILON = 1.0E-4;
 
     public static void main(String[] args){
+        System.out.println("args=" + Arrays.toString(args));
+        States states = new States(args[1]);
+        Symbols symbols = new Symbols(args[2]);
+        Queries queries = new Queries(args[3]);
 
+        System.out.println(states.toString());
+        System.out.println(symbols.toString());
+        System.out.println(queries.toString());
     }
 }
--- src/Queries.java
+++ src/Queries.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Andrew
@@ -6,8 +12,38 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Queries {
+    private ArrayList<ArrayList<String>> tokens = new ArrayList<ArrayList<String>>();
 
-    public Queries(){
+    public Queries(String fileName){
+        Scanner scanner;
+        Scanner lineScanner;
+        File file = new File(fileName);
+        String line;
+        ArrayList<String> tokenList;
 
+        try {
+            scanner = new Scanner(file);
+
+            while(scanner.hasNextLine()){
+                line = scanner.nextLine();
+                lineScanner = new Scanner(line);
+                tokenList = new ArrayList<String>();
+                while(lineScanner.hasNext()){
+                    tokenList.add(lineScanner.next());
+                }
+                tokens.add(tokenList);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Query file not found...");
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Queries{" +
+                "tokens=" + tokens +
+                '}';
     }
 }
